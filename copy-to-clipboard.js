06b068fbf3fce0693a4b4201bbf9ac5f9ee0459a
marioseixas <seixas@ib.bsb.br>
2024-01-06 16:42:28 -0300

    Update copy-to-clipboard.js

--- copy-to-clipboard.js
+++ copy-to-clipboard.js
@@ -1,19 +1,22 @@
-document.addEventListener('DOMContentLoaded', function() {
-  document.querySelectorAll('.copy-button').forEach(function(button) {
-    button.addEventListener('click', function() {
-      var codeContainer = this.closest('.code-container');
-      var codeBlock = codeContainer ? codeContainer.querySelector('code') : null;
-      if (codeBlock) {
-        var textToCopy = codeBlock.innerText.trim();
-        navigator.clipboard.writeText(textToCopy).then(function() {
-          button.textContent = 'Copied!';
-          setTimeout(function() { button.textContent = 'Copy'; }, 2000);
-        }).catch(function(err) {
-          console.error('Error copying text: ', err);
-        });
-      } else {
-        console.error("Couldn't find the code block associated with the copy button.");
-      }
+document.addEventListener('DOMContentLoaded', function () {
+  var highlightBlocks = document.querySelectorAll('pre.highlight, div.highlight');
+
+  highlightBlocks.forEach(function(block) {
+    var button = document.createElement('button');
+    button.className = 'copy-button';
+    button.textContent = 'Copy';
+    button.title = 'Copy to clipboard';
+    button.addEventListener('click', function () {
+      // Now we have to find just the plain text within this code block without line numbers or other additions
+      var code = block.querySelector('code').innerText;
+      navigator.clipboard.writeText(code).then(function () {
+        button.textContent = 'Copied!';
+        setTimeout(function () { button.textContent = 'Copy'; }, 2000);
+      }).catch(function (err) {
+        console.error('Error copying text: ', err);
+      });
     });
+
+    block.appendChild(button);
   });
 });
