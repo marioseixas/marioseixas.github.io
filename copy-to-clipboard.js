f1d46ee92ee4de944f0fb53bf4dedca0ceff9b52
marioseixas <seixas@ib.bsb.br>
2024-01-06 17:11:38 -0300

    Update copy-to-clipboard.js

--- copy-to-clipboard.js
+++ copy-to-clipboard.js
@@ -1,5 +1,5 @@
 document.addEventListener('DOMContentLoaded', function () {
-  var highlightBlocks = document.querySelectorAll('code');
+  var highlightBlocks = document.querySelectorAll('pre.highlight');
 
   highlightBlocks.forEach(function(block) {
     var button = document.createElement('button');
