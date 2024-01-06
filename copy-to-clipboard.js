38bd398a0d47494e439cee2b7f08f99d02961170
marioseixas <seixas@ib.bsb.br>
2024-01-06 16:25:53 -0300

    Update copy-to-clipboard.js

--- copy-to-clipboard.js
+++ copy-to-clipboard.js
@@ -1,19 +1,19 @@
-document.querySelectorAll('.copy-button').forEach(function(button) {
-  button.addEventListener('click', function() {
-    // Find the parent .code-container then find the contained <code>
-    var codeContainer = this.closest('.code-container');
-    var codeBlock = codeContainer ? codeContainer.querySelector('code') : null;
-    if (codeBlock) {
-      // Now we have a reference to the <code>, continue as before
-      var textToCopy = codeBlock.innerText.trim();
-      navigator.clipboard.writeText(textToCopy).then(function() {
-        button.textContent = 'Copied!';
-        setTimeout(function() { button.textContent = 'Copy'; }, 2000);
-      }).catch(function(err) {
-        console.error('Error copying text: ', err);
-      });
-    } else {
-      console.error("Couldn't find the code block associated with the copy button.");
-    }
+document.addEventListener('DOMContentLoaded', function() {
+  document.querySelectorAll('.copy-button').forEach(function(button) {
+    button.addEventListener('click', function() {
+      var codeContainer = this.closest('.code-container');
+      var codeBlock = codeContainer ? codeContainer.querySelector('code') : null;
+      if (codeBlock) {
+        var textToCopy = codeBlock.innerText.trim();
+        navigator.clipboard.writeText(textToCopy).then(function() {
+          button.textContent = 'Copied!';
+          setTimeout(function() { button.textContent = 'Copy'; }, 2000);
+        }).catch(function(err) {
+          console.error('Error copying text: ', err);
+        });
+      } else {
+        console.error("Couldn't find the code block associated with the copy button.");
+      }
+    });
   });
 });
