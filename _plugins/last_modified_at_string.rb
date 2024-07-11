f28eb875ca82e58bcbef1cd139d38ef64c0746b5
marioseixas <seixas@ib.bsb.br>
2024-07-11 01:31:19 -0300

    Update last_modified_at_string.rb

--- _plugins/last_modified_at_string.rb
+++ _plugins/last_modified_at_string.rb
@@ -3,7 +3,7 @@ require 'jekyll-last-modified-at'
 module Jekyll
   class LastModifiedAtStringGenerator < Jekyll::Generator
     def generate(site)
-      site.posts.docs.each do |post|
+      site.posts.each do |post|
         post.data['last_modified_at_str'] = Jekyll::LastModifiedAt::Determinator.new(site.source, post.path, '%Y-%m-%d %H:%M:%S').to_s
       end
     end
