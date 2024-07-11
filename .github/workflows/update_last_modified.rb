516f232fa6e363b77c29f05a5ea13810c86b2ca5
marioseixas <seixas@ib.bsb.br>
2024-07-10 23:17:06 -0300

    Update update_last_modified.rb

--- .github/workflows/update_last_modified.rb
+++ .github/workflows/update_last_modified.rb
@@ -1,14 +1,8 @@
 #!/usr/bin/env ruby
 require 'yaml'
 require 'find'
-require 'time'
-def ensure_time(value)
-  return Time.parse(value).strftime('%Y-%m-%d %H:%M:%S %z') if value.is_a?(String)
-  return value.strftime('%Y-%m-%d %H:%M:%S %z') if value.is_a?(Time)
-  Time.now.strftime('%Y-%m-%d %H:%M:%S %z')
-end
-Find.find('.') do |path|
-  next unless File.file?(path) && ['.md', '.html'].include?(File.extname(path))
+Find.find('./_posts') do |path|
+  next unless File.file?(path) && File.extname(path) == '.md'
   content = File.read(path)
   if content =~ /\A(---\s*\n.*?\n?)^(---\s*$\n?)/m
     front_matter = YAML.safe_load($1)
@@ -16,9 +10,7 @@ Find.find('.') do |path|
   else
     next
   end
-  git_date = `git log -1 --format=%cd --date=iso -- #{path}`.strip
-  last_modified = git_date.empty? ? File.mtime(path) : Time.parse(git_date)
-  
-  front_matter['last_modified_at'] = ensure_time(last_modified)
+  # Use Unix timestamp (seconds since epoch)
+  front_matter['last_modified_at'] = File.mtime(path).to_i
   File.write(path, "---\n#{front_matter.to_yaml}---\n#{content_body}")
 end
