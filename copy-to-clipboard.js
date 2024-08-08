e09a6f2325436afdea59b90e6716af4b4d534a20
marioseixas <seixas@ib.bsb.br>
2024-08-08 10:02:46 -0300

    Update copy-to-clipboard.js

--- copy-to-clipboard.js
+++ copy-to-clipboard.js
@@ -3,18 +3,18 @@ document.addEventListener('DOMContentLoaded', () => {
 
   copyButtons.forEach(button => {
     button.addEventListener('click', () => {
-      const codeBlock = button.closest('pre');
+      const codeBlock = button.closest('pre > code');
       if (!codeBlock) return;
 
-      const codeText = codeBlock.innerText.trim();
+      const codeText = codeBlock.textContent.trim();
       copyToClipboard(codeText)
         .then(() => {
           button.classList.add('copied');
-          button.innerText = 'Copied!';
+          button.textContent = 'Copied!';
 
           setTimeout(() => {
             button.classList.remove('copied');
-            button.innerText = 'Copy';
+            button.textContent = 'Copy';
           }, 2000);
         })
         .catch(err => console.error('Failed to copy text: ', err));
@@ -22,20 +22,31 @@ document.addEventListener('DOMContentLoaded', () => {
   });
 });
 
-function copyToClipboard(text) {
-  return navigator.clipboard.writeText(text).catch(err => {
+async function copyToClipboard(text) {
+  try {
+    await navigator.clipboard.writeText(text);
+  } catch (err) {
     console.error('Async: Could not copy text: ', err);
     // Fallback for older browsers
-    const textArea = document.createElement('textarea');
-    textArea.value = text;
-    textArea.style.position = 'fixed';  // Prevent scrolling to bottom
-    document.body.appendChild(textArea);
-    textArea.focus();
-    textArea.select();
+    await fallbackCopyToClipboard(text);
+  }
+}
+
+function fallbackCopyToClipboard(text) {
+  const textArea = document.createElement('textarea');
+  textArea.value = text;
+  textArea.style.position = 'fixed';  // Prevent scrolling to bottom
+  document.body.appendChild(textArea);
+  textArea.focus();
+  textArea.select();
+
+  return new Promise((resolve, reject) => {
     try {
-      document.execCommand('copy');
+      const successful = document.execCommand('copy');
+      resolve();
     } catch (err) {
       console.error('Fallback: Oops, unable to copy', err);
+      reject(err);
     }
     document.body.removeChild(textArea);
   });
