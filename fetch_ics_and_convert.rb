87c5abc43b363c871d56d7d8b05f811db231a303
marioseixas <seixas@ib.bsb.br>
2024-08-13 14:55:42 -0300

    Update fetch_ics_and_convert.rb

--- fetch_ics_and_convert.rb
+++ fetch_ics_and_convert.rb
@@ -1,39 +1,38 @@
-require 'bundler/setup'
 require 'net/http'
 require 'uri'
 require 'icalendar'
 require 'json'
-require 'yaml'
-require 'digest'
+require 'digest/md5'
 
-uri = URI('https://dav.heydola.com/dav.php/calendars/py28wj81/default/?export')
+url = "https://dav.heydola.com/dav.php/calendars/py28wj81/default/?export"
+username = "py28wj81"
+password = "20287290"
 
-# Create HTTP object and request
+uri = URI(url)
 http = Net::HTTP.new(uri.host, uri.port)
 http.use_ssl = true
-request = Net::HTTP::Get.new(uri.request_uri)
-request.basic_auth('py28wj81', '20287290')
 
-# Fetch the calendar data
+request = Net::HTTP::Get.new(uri.request_uri)
+request.basic_auth(username, password)
+
 response = http.request(request)
 
-if response.code == '200'
-  cals = Icalendar::Calendar.parse(response.body)
-  events = cals.flat_map(&:events).map do |event|
+if response.code.to_i == 200
+  puts "Access successful!"
+  calendar = Icalendar::Calendar.parse(response.body).first
+
+  events = calendar.events.map do |event|
     {
-      id: Digest::MD5.hexdigest(event.uid),
-      title: event.summary,
-      start: event.dtstart.to_s,
-      end: event.dtend.to_s,
-      location: event.location,
-      description: event.description
+      "title" => event.summary,
+      "start" => event.dtstart.to_time.iso8601,
+      "end" => event.dtend.to_time.iso8601
     }
   end
 
-  # Save events to JSON file
-  File.open('_site/assets/data/events.json', 'w') do |file|
-    file.write(JSON.pretty_generate(events))
+  File.open("events.json", "w") do |f|
+    f.write(JSON.pretty_generate(events))
   end
 else
-  puts "Failed to fetch the calendar: #{response.message}"
+  puts "Failed to access. Status code: #{response.code}"
+  puts response.body
 end
