2fd2580af52e9cc2555601e3070c773243bfd084
marioseixas <seixas@ib.bsb.br>
2024-08-16 20:04:20 -0300

    Update pagefind.rb

--- _plugins/pagefind.rb
+++ _plugins/pagefind.rb
@@ -1,3 +1,3 @@
 Jekyll::Hooks.register :site, :post_write do |site|
-    `npm exec -- pagefind --site ./github-pages-build/artifact --output-path pagefind`
+    `npm exec -- pagefind --site ./github-pages-build/artifact --output-path ./github-pages-build/artifact/_site/pagefind`
 end
