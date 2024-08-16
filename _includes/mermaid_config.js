64dc0ee9576700841e6c56ca98458163cb5c04ae
marioseixas <seixas@ib.bsb.br>
2024-08-16 09:22:23 -0300

    Update mermaid_config.js

--- _includes/mermaid_config.js
+++ _includes/mermaid_config.js
@@ -1,1 +1,13 @@
-{}
+{
+  startOnLoad: true,
+  theme: window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches ? 'dark' : 'default',
+  flowchart: {
+    curve: 'basis',
+    useMaxWidth: true,
+    rankSpacing: 40,
+    nodeSpacing: 40,
+    padding: 5,
+    htmlLabels: true
+  },
+  securityLevel: 'loose'
+}
