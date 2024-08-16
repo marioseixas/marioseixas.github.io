bc50b80da31aa74256bf17d41f9081f25982ee9d
marioseixas <seixas@ib.bsb.br>
2024-08-16 10:03:51 -0300

    Update mermaid_config.js

--- _includes/mermaid_config.js
+++ _includes/mermaid_config.js
@@ -1,13 +1,1 @@
-{
-  startOnLoad: true,
-  theme: 'default',
-  flowchart: {
-    curve: 'basis',
-    useMaxWidth: true,
-    rankSpacing: 40,
-    nodeSpacing: 40,
-    padding: 5,
-    htmlLabels: true
-  },
-  securityLevel: 'loose'
-}
+{}
