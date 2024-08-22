066af6ee044044743a882bb73d5c6d317a0a5e82
marioseixas <seixas@ib.bsb.br>
2024-08-21 23:10:01 -0300

    Create tags.py

---
+++ tags.py
@@ -0,0 +1,41 @@
+import os
+import yaml
+from collections import defaultdict
+
+def process_tags(posts_dir, output_file):
+    tag_data = defaultdict(list)
+    
+    for filename in os.listdir(posts_dir):
+        if filename.endswith('.md'):
+            with open(os.path.join(posts_dir, filename), 'r') as f:
+                frontmatter = ''
+                for line in f:
+                    if line.strip() == '---':
+                        if frontmatter:
+                            break
+                        frontmatter = '---\n'
+                    else:
+                        frontmatter += line
+                
+                post_data = yaml.safe_load(frontmatter)
+                tags = post_data.get('tags', [])
+                
+                if isinstance(tags, str):
+                    tags = [tag.strip() for tag in tags.split(',')]
+                elif not isinstance(tags, list):
+                    tags = [str(tags)]
+                
+                title = post_data.get('title', filename.replace('.md', ''))
+                url = '/' + filename.replace('.md', '')
+                
+                for tag in tags:
+                    tag_parts = tag.split('>')
+                    for i in range(len(tag_parts)):
+                        partial_tag = '>'.join(tag_parts[:i+1])
+                        tag_data[partial_tag].append({'title': title, 'url': url})
+
+    with open(output_file, 'w') as f:
+        yaml.dump([{'tag': tag, 'posts': posts} for tag, posts in tag_data.items()], f)
+
+if __name__ == '__main__':
+    process_tags('_posts', '_data/processed_tags.yml')
