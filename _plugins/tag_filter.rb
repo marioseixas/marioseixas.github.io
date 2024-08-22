fe5c9cbd0110bbc472483a8c09b88cc1812b5721
marioseixas <seixas@ib.bsb.br>
2024-08-21 22:01:12 -0300

    Update tag_filter.rb

--- _plugins/tag_filter.rb
+++ _plugins/tag_filter.rb
@@ -2,8 +2,11 @@ module Jekyll
   module TagFilter
     def contains_tag(tags, tag)
       tags = tags.first if tags.is_a?(Array) && tags.first.is_a?(Array) # Handle nested arrays
-      tags = tags.map(&:strip) if tags.is_a?(Array)
-      tags.any? { |t| t == tag || t.start_with?(tag + ">") }
+      tags = tags.join(',').split(',').map(&:strip) if tags.is_a?(Array)
+      tag_parts = tag.split('>')
+      tags.any? do |t| 
+        t == tag || tag_parts.all? { |part| t.include?(part) } 
+      end
     end
   end
 end
