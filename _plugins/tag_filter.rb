f8d7a6a1bbb8a85da21f6775fbf67b3a53ff4ef1
marioseixas <seixas@ib.bsb.br>
2024-08-21 22:24:23 -0300

    Create tag_filter.rb

---
+++ _plugins/tag_filter.rb
@@ -0,0 +1,14 @@
+module Jekyll
+  module TagFilter
+    def contains_tag(tags, tag)
+      tags = tags.first if tags.is_a?(Array) && tags.first.is_a?(Array)
+      tags = tags.join(',').split(',').map(&:strip) if tags.is_a?(Array)
+      tag_parts = tag.split('>') 
+      tags.any? do |t| 
+        t == tag || tag_parts.all? { |part| t.include?(part) }
+      end
+    end
+  end
+end
+
+Liquid::Template.register_filter(Jekyll::TagFilter)
