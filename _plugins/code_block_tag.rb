74ccf2344a8811579614d642994c13545acb341f
marioseixas <seixas@ib.bsb.br>
2024-09-02 18:40:52 -0300

    Update code_block_tag.rb

--- _plugins/code_block_tag.rb
+++ _plugins/code_block_tag.rb
@@ -6,16 +6,16 @@ module Jekyll
     end
 
     def render(context)
-      code_content = super.strip.gsub(/^\s+/, "")
-      filename = "code-block.#{@language}"
+      code_content = super.strip.gsub(/^\s+/, "") # Remove leading whitespace
+      filename = "code-block.#{@language}"         # Create a meaningful filename 
       download_label = "Download #{@language.capitalize()} Code"
 
-      # Encode the code content for use in a data URI
-      encoded_content = URI.encode_www_form_component(code_content)
+      # Encode code content for use in a data URI
+      encoded_content = URI.encode_www_form_component(code_content) 
       data_uri = "data:text/plain;charset=utf-8,#{encoded_content}"
 
-      # Construct the HTML output with the data URI
-      "<section data-src=\"#{filename}\" data-download-link data-download-link-label=\"#{download_label}\" class=\"language-#{@language}\"><code class=\"language-#{@language}\">#{code_content}</code></section>"
+      # Construct the HTML output with the data URI in data-src
+      "<section data-src=\"#{data_uri}\" data-download-link data-download-link-label=\"#{download_label}\" class=\"language-#{@language}\"><code class=\"language-#{@language}\">#{code_content}</code></section>"
     end
   end
 end
