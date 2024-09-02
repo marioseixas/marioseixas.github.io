d18c628d62154ca82c7d2deb11d8617a21fb8a26
marioseixas <seixas@ib.bsb.br>
2024-09-02 18:35:03 -0300

    Update code_block_tag.rb

--- _plugins/code_block_tag.rb
+++ _plugins/code_block_tag.rb
@@ -2,20 +2,19 @@ module Jekyll
   class CodeBlockTag < Liquid::Block
     def initialize(tag_name, markup, tokens)
       super
-      @language = markup.strip # Extract the language from the tag arguments
+      @language = markup.strip
     end
 
     def render(context)
-      # Extract the code block content while handling indentation
       code_content = super.strip.gsub(/^\s+/, "")
-
-      # Build data-src attribute based on language 
       filename = "code-block.#{@language}"
-
-      # Build data-download-link-label attribute
       download_label = "Download #{@language.capitalize()} Code"
 
-      # Construct the HTML output
+      # Encode the code content for use in a data URI
+      encoded_content = URI.encode_www_form_component(code_content)
+      data_uri = "data:text/plain;charset=utf-8,#{encoded_content}"
+
+      # Construct the HTML output with the data URI
       "<section data-src=\"#{filename}\" data-download-link data-download-link-label=\"#{download_label}\" class=\"language-#{@language}\"><code class=\"language-#{@language}\">#{code_content}</code></section>"
     end
   end
