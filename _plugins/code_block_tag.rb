7b03af1fc402ba409cfa690545472d0fe37159a4
marioseixas <seixas@ib.bsb.br>
2024-09-10 11:17:53 -0300

    Update code_block_tag.rb

--- _plugins/code_block_tag.rb
+++ _plugins/code_block_tag.rb
@@ -41,7 +41,7 @@ module Jekyll
       escaped_code_content = code_content.gsub('"', '&quot;')
       
       # Construct HTML output with data attributes 
-      "<aside class=\"line-numbers\"><section data-filename=\"#{filename}\" data-code=\"#{escaped_code_content}\" data-download-link data-download-link-label=\"#{download_label}\" class=\"language-#{@language}\" style=\"white-space:pre-wrap;\"><code class=\"language-#{@language}\">#{code_content}</code></section></aside>"
+      "<section data-filename=\"#{filename}\" data-code=\"#{escaped_code_content}\" data-download-link data-download-link-label=\"#{download_label}\" class=\"language-#{@language} line-numbers\" style=\"white-space:pre-wrap;\"><code class=\"language-#{@language}\">#{code_content}</code></section>"
     end
   end
 end
