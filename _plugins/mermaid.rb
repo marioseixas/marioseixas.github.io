418a81e1ed81999d16e92eba9a83e4562c2fbe9b
marioseixas <seixas@ib.bsb.br>
2024-09-10 17:52:58 -0300

    Update mermaid.rb

--- _plugins/mermaid.rb
+++ _plugins/mermaid.rb
@@ -3,32 +3,35 @@
 module Jekyll
   class RenderMermaid < Liquid::Block
     def render(context)
-      # Capture the content inside the block and strip unnecessary whitespace
       text = super.strip
-
-      # Wrap the content inside a <div> with appropriate classes and styles
       mermaid_script = generate_mermaid_script
-      mermaid_div = "<div class='mermaid' style='white-space: pre-wrap;'>#{text}</div>"
 
-      # Return the script and div as a single string without any additional processing
-      "#{mermaid_script}#{mermaid_div}"
+      <<~HTML
+        <div class='mermaid' style='white-space: pre-wrap;'>#{text}</div>
+        <script src="https://cdn.jsdelivr.net/npm/svg-pan-zoom@3.6.1/dist/svg-pan-zoom.min.js"></script>
+        <script type='module'>
+          import mermaid from 'https://cdn.jsdelivr.net/npm/mermaid@10/dist/mermaid.esm.min.mjs';
+          #{mermaid_script}
+        </script>
+      HTML
     end
 
     private
 
     def generate_mermaid_script
       <<~SCRIPT
-        <script src="https://cdn.jsdelivr.net/npm/svg-pan-zoom@3.6.1/dist/svg-pan-zoom.min.js"></script>
-        <script type='module'>
-          import mermaid from 'https://cdn.jsdelivr.net/npm/mermaid@10/dist/mermaid.esm.min.mjs';
-          mermaid.initialize({
-            startOnLoad: true,
-            look: 'handDrawn',
-            theme: 'neo-dark',
-            er: { useMaxWidth: true }
-          });
-          setTimeout(() => {
-            svgPanZoom(document.querySelector("div.mermaid>svg"), {
+        const mermaidConfig = {
+          startOnLoad: true,
+          look: 'handDrawn',
+          theme: 'neo-dark',
+          er: { useMaxWidth: true }
+        };
+        mermaid.initialize(mermaidConfig);
+
+        setTimeout(() => {
+          const svgElement = document.querySelector("div.mermaid>svg");
+          if (svgElement) {
+            svgPanZoom(svgElement, {
               minZoom: 0.5,
               maxZoom: 10,
               fit: true,
@@ -37,12 +40,11 @@ module Jekyll
               center: true,
               refreshRate: "auto",
             });
-          }, 200);
-        </script>
+          }
+        }, 200);
       SCRIPT
     end
   end
 end
 
-# Register the custom tag with Liquid
 Liquid::Template.register_tag('mermaid', Jekyll::RenderMermaid)
